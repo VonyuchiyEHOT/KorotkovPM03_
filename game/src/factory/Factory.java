253301a03ad183645ecaa51ab0e86a94641bb0bb
Kostya15 <akarachoklove@gmail.com>
2026-01-18 21:45:52 +0300

    Update: Factory.java changes, added images, private.xml update

--- game/src/factory/Factory.java
+++ game/src/factory/Factory.java
@@ -11,49 +11,27 @@ public class Factory extends JFrame {
     int boxX = 200;
     int boxY = 0;
     
-    Image factoryImage = null;
-    Image boxImage = null;
+    ImageIcon factoryIcon = null;
+    ImageIcon boxIcon = null;
+    ImageIcon logoIcon = null;
     
     public static void main(String[] args) {
         new Factory();
     }
     
     public Factory() {
-        setTitle("Фабрика");
+        setTitle("Производственная компания");
         setSize(800, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         
-        // ЗАГРУЗКА КАРТИНОК ВСЕМИ СПОСОБАМИ
+        // Загрузка картинок
         try {
-            // Способ 1: Через класс (самый надежный)
-            factoryImage = Toolkit.getDefaultToolkit().getImage(getClass().getResource("factory.png"));
-            boxImage = Toolkit.getDefaultToolkit().getImage(getClass().getResource("box.png"));
-            
-            // Ждем загрузки
-            MediaTracker tracker = new MediaTracker(this);
-            if (factoryImage != null) tracker.addImage(factoryImage, 0);
-            if (boxImage != null) tracker.addImage(boxImage, 1);
-            tracker.waitForAll();
-            
-        } catch (Exception e1) {
-            try {
-                // Способ 2: Простые пути
-                factoryImage = new ImageIcon("factory.png").getImage();
-                boxImage = new ImageIcon("box.png").getImage();
-            } catch (Exception e2) {
-                // Способ 3: Из src
-                factoryImage = new ImageIcon("src/factory/factory.png").getImage();
-                boxImage = new ImageIcon("src/factory/box.png").getImage();
-            }
-        }
-        
-        // Проверка
-        if (factoryImage == null) {
-            System.out.println("ВНИМАНИЕ: Картинки не загружены!");
-            System.out.println("Создай factory.png и box.png в папке с Factory.java");
-        } else {
-            System.out.println("Картинки загружены!");
+            factoryIcon = new ImageIcon(getClass().getResource("factory.png"));
+            boxIcon = new ImageIcon(getClass().getResource("box.png"));
+            logoIcon = new ImageIcon(getClass().getResource("logo.png"));
+        } catch (Exception e) {
+            // Если не загрузились - ничего страшного
         }
         
         // Панель игры
@@ -63,66 +41,78 @@ public class Factory extends JFrame {
                 super.paintComponent(g);
                 
                 // Фон
-                if (factoryImage != null) {
-                    g.drawImage(factoryImage, 0, 0, getWidth(), getHeight(), this);
+                if (factoryIcon != null) {
+                    g.drawImage(factoryIcon.getImage(), 0, 0, getWidth(), getHeight(), this);
                 } else {
-                    // Рисуем фабрику цветами
-                    g.setColor(new Color(135, 206, 235)); // небо
+                    g.setColor(new Color(173, 216, 230));
                     g.fillRect(0, 0, getWidth(), getHeight());
-                    
-                    g.setColor(Color.DARK_GRAY); // здание
-                    g.fillRect(100, 250, 600, 250);
-                    
-                    g.setColor(Color.YELLOW); // окна
-                    for (int i = 0; i < 5; i++) {
-                        g.fillRect(150 + i*110, 280, 40, 40);
+                    g.setColor(Color.DARK_GRAY);
+                    g.fillRect(150, 250, 500, 200);
+                    g.setColor(Color.YELLOW);
+                    for (int i = 0; i < 6; i++) {
+                        g.fillRect(180 + i*80, 280, 40, 40);
                     }
-                    
-                    g.setColor(Color.BLACK); // труба
-                    g.fillRect(400, 200, 40, 50);
-                    g.setColor(Color.GRAY); // дым
-                    g.fillOval(390, 180, 60, 30);
+                }
+                
+                // Логотип
+                if (logoIcon != null) {
+                    int logoWidth = 150;
+                    int logoHeight = 150;
+                    int logoX = getWidth()/2 - logoWidth/2;
+                    g.drawImage(logoIcon.getImage(), logoX, 10, logoWidth, logoHeight, this);
+                } else {
+                    g.setColor(Color.BLUE);
+                    g.setFont(new Font("Arial", Font.BOLD, 36));
+                    String logoText = "ПРОИЗВОДСТВЕННАЯ КОМПАНИЯ";
+                    int textWidth = g.getFontMetrics().stringWidth(logoText);
+                    g.drawString(logoText, getWidth()/2 - textWidth/2, 60);
                 }
                 
                 // Ящик
-                if (boxImage != null) {
-                    g.drawImage(boxImage, boxX, boxY, 60, 60, this);
+                if (boxIcon != null) {
+                    g.drawImage(boxIcon.getImage(), boxX, boxY, 80, 80, this);
                 } else {
-                    g.setColor(new Color(160, 82, 45)); // коричневый
-                    g.fillRect(boxX, boxY, 60, 60);
+                    g.setColor(new Color(160, 82, 45));
+                    g.fillRect(boxX, boxY, 80, 80);
                     g.setColor(Color.WHITE);
-                    g.drawRect(boxX + 5, boxY + 5, 50, 50);
-                    g.drawString("BOX", boxX + 15, boxY + 35);
+                    g.setFont(new Font("Arial", Font.BOLD, 20));
+                    g.drawString("ЯЩИК", boxX + 15, boxY + 50);
                 }
                 
-                // Текст
+                // Информация
                 g.setColor(Color.BLACK);
                 g.setFont(new Font("Arial", Font.BOLD, 24));
-                g.drawString("Продукция: " + products, 20, 40);
-                g.drawString("Деньги: $" + (products * 100), 20, 80);
-                g.drawString("Кликай на ящики!", 20, 120);
+                g.drawString("Продукция: " + products, 30, 120);
+                g.drawString("Финансы: $" + (products * 100), 30, 160);
+                g.drawString("Персонал: " + (5 + products/3), 30, 200);
+                g.drawString("Грузовики: " + (2 + products/5), 30, 240);
+                
+                // Инструкция
+                g.setColor(Color.white);
+                g.setFont(new Font("Arial", Font.BOLD, 18));
+                g.drawString("КЛИКАЙТЕ НА ПАДАЮЩИЕ ЯЩИКИ", getWidth()/2 - 180, getHeight() - 50);
             }
         };
         
-        // Клик
+        // Клик по ящику
         panel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 int x = e.getX();
                 int y = e.getY();
                 
-                if (x >= boxX && x <= boxX + 60 && 
-                    y >= boxY && y <= boxY + 60) {
+                if (x >= boxX && x <= boxX + 80 && y >= boxY && y <= boxY + 80) {
                     products++;
                     boxY = 0;
                     boxX = new Random().nextInt(700);
+                    setTitle("Производство: " + products + " | Финансы: $" + (products * 100));
                     repaint();
                 }
             }
         });
         
-        // Таймер
-        Timer timer = new Timer(50, new ActionListener() {
+        // Таймер для анимации
+        Timer timer = new Timer(30, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 boxY += 5;
